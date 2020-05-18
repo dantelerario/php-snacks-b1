c66e4c439ec68400dee399e5972e10c989a61dd0
Dante L'Erario <dantelerario@gmail.com>
2020-05-18 14:27:16 +0200

    snack 2 done

--- index.php
+++ index.php
@@ -63,6 +63,45 @@
        <?php endfor; ?>
      </ul>
 
+     <h1>PHP Snack 2</h1>
+     <?php
+     // PHP Snack 2:
+     //  Passare come parametri GET name, mail e age e verificare (cercando i metodi che nonconosciamo nella documentazione) che:
+     //  1. name sia più lungo di 3 caratteri,
+     //  2. che mail contenga un punto e una chiocciola
+     //  3. e che age sia un numero.Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+     $dati = $_GET;
+     $accesso = 'non riuscito';
+
+
+     //check presenza valori
+
+     if ( empty($dati['name']) || empty($dati['mail']) || empty($dati['age']) ) {
+       $accesso = 'inserire tutti i parametri per accede';
+     }
+
+     //check name
+     elseif ( strlen($dati['name']) <= 3 ) {
+       $accesso = 'non riuscito';
+     }
+     //check mail
+     elseif ( strpos($dati['mail'], '@' ) === false || strpos($dati['mail'], '.' ) === false ) {
+       $accesso = 'non riuscito';
+     }
+     //check age
+     elseif ( ! is_numeric($dati['age']) ) {
+       $accesso = 'non riuscito';
+     }
+     else {
+       $accesso = 'riuscito';
+     }
+
+      ?>
+
+    <h2>ACCESSO: <?php echo $accesso; ?></h2>
+
+
 
   </body>
 </html>
